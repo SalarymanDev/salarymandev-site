fa4b56d639e683b98f544b4aaad875793636d07b
Robert Kennedy <robert076kennedy@gmail.com>
2025-03-09 21:47:12 -0400

    Created the projects page with all my current projects

--- src/app/company/page.tsx
+++ src/app/company/page.tsx
@@ -1,3 +1,33 @@
+// import Image from "next/image";
+
 export default async function Company() {
-  return <></>;
+  return (
+    <main className="flex flex-col items-center justify-center text-white">
+      <div className="container flex flex-col items-center justify-center gap-6 px-4 py-4">
+        <div className="flex flex-col items-center justify-center pb-2">
+          {/* <Image
+            alt="Profile Picture"
+            width={200}
+            height={200}
+            src="/profile.png"
+            className="mr-8 h-20 w-auto"
+          ></Image> */}
+          <h1 className="text-3xl font-bold">Salaryman LLC</h1>
+          <h3 className="text-xl">Test</h3>
+          {/* <div className="items-begin flex shrink-0 flex-col justify-center">
+            <h1 className="text-3xl font-bold">Salaryman LLC</h1>
+            <h2 className="text-xl font-bold">SalarymanDev</h2>
+            <h3 className="text-md font-bold">Founder of Salaryman, LLC</h3>
+          </div> */}
+        </div>
+        <div className="grid grid-cols-2 gap-6 p-2">
+          {/* <p className="col-span-2">
+            {
+              "Yo, I'm Robert an ex-Amazon software engineer from the United States currently living in Tokyo, Japan."
+            }
+          </p> */}
+        </div>
+      </div>
+    </main>
+  );
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,3 +1,57 @@
 export default async function Projects() {
-  return <></>;
+  return (
+    <main className="flex flex-col items-center justify-center text-white">
+      <div className="container flex flex-col items-center justify-center gap-6 px-4 py-4">
+        <h1 className="text-3xl font-bold">Projects</h1>
+
+        <div className="grid grid-cols-2">
+          <div className="flex flex-col p-4">
+            <h1 className="text-2xl font-bold">Backcountry</h1>
+            <p>
+              Backcountry is a video game where you backpack a scenic mountain
+              range and take pictures of nature. From your cabin in the
+              mountains you must plan and prepare your food, water, and
+              equipment to trek deep into the mountains.
+            </p>
+            <p className="pt-4">Created using Unreal Engine 5</p>
+          </div>
+
+          <div className="flex flex-col p-4">
+            <h1 className="text-2xl font-bold">Floating Star</h1>
+            <p>
+              Help bring color back in this pastel sci-fi world where you travel
+              around the galaxy helping those you find. The color in the world
+              is reflected by emotions becoming grey around those who are sad or
+              virbant around those that are happy.
+            </p>
+            <p className="pt-4">Created using Godot 4.4</p>
+          </div>
+
+          <div className="flex flex-col p-4">
+            <h1 className="text-2xl font-bold">Salaryman AI Coder</h1>
+            <p>
+              This is a GitHub Application that will automatically write code
+              and submit pull requests for any repository issues assigned to it.
+            </p>
+            <p className="pt-4">
+              Created using NextJS, Vercel, GitHub Apps, and Redis.
+            </p>
+          </div>
+
+          <div className="flex flex-col p-4">
+            <h1 className="text-2xl font-bold">TASTE</h1>
+            <p>
+              The Anime Soundtrack Testing Establishment (TASTE) is database for
+              my friends and I to track and rate anime opening/outro music to
+              determine if they are certified bangers, or not. This includes
+              TASTE board members being able to vote yea or nay on each song.
+            </p>
+            <p className="pt-4">
+              Created using NextJS, Vercel, and Postgresql.
+            </p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
